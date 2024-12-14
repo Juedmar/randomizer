22f8298fb0d488436cd1007e3c966d2838bc8da2
Juan Eduardo Méndez Arias <116532250+Juedmar@users.noreply.github.com>
2024-12-14 16:17:02 -0300

    Add files via upload

---
+++ files/randomizeScripts.js
@@ -0,0 +1,59 @@
+
+// RANDOMIZE FUNCTION
+// RANDOMIZE FUNCTION
+// RANDOMIZE FUNCTION
+
+
+// GLOBAL AUDIO VARIABLE!
+var audioToggle;
+
+
+// AUDIO FUNCTIONS!
+function enableAudio() {
+document.getElementById("audioOn").style.display = "none";
+document.getElementById("audioOff").style.display = "block";
+audioToggle = "On";
+}
+
+function disableAudio() {
+document.getElementById("audioOn").style.display = "block";
+document.getElementById("audioOff").style.display = "none";
+audioToggle = "Off";
+}
+
+
+// GLOBAL ANSWER VARIABLE
+var theAnswer;
+
+
+function goRandomize() {
+document.getElementById("answerButton").style.display = "block";
+var questAmount = document.getElementById('questAmount').value;
+var randomQs = document.getElementById("randomQs");
+
+// RANDOMIZING VARIABLE & UPDATING HTM ELEMENT!
+let random = "q" + Math.floor(Math.random()*(questAmount-0)+1);
+// randomQs.innerHTML = "<b>Pregunta N°" + random.slice(1) +":</b> " + document.getElementById(random).value ; // ORIGINAL CODE, NO TIMEOUT
+randomQs.innerHTML = "🎲";
+setTimeout(function() { randomQs.innerHTML = "<b>Pregunta N°" + random.slice(1) +":</b> " + document.getElementById(random).value }, 500)
+theAnswer = "a" + random.slice(1);
+
+// PLAY DICE SHUFFLE AUDIO!
+if (audioToggle === "On") { 
+ document.getElementById("shuffle").play();
+ }
+}
+
+
+function getAnswer() {
+var thisAnswer = theAnswer;
+var randomQs = document.getElementById("randomQs");
+// document.getElementById("randomQs").innerHTML = "<b>Respuesta:</b> " + document.getElementById(thisAnswer).value + "!"; // ORIGNAL ANSWER, NO TIMEOUT!
+document.getElementById("randomQs").innerHTML = "📖";
+setTimeout(function() { document.getElementById("randomQs").innerHTML = "<b>Respuesta:</b> " + document.getElementById(thisAnswer).value + "!" }, 400)
+
+// PLAY ANSWER EFFECT AUDIO!
+if (audioToggle === "On") { 
+ document.getElementById("answer").play();
+ }
+}
